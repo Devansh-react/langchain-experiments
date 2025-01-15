7b029c10ef062430d2742c24851b52445e3190fd
Devansh-react <devanshs748@gmail.com>
2025-01-15 20:34:29 +0530

    check -youtube.py

--- youtube/youtube_chat.py
+++ youtube/youtube_chat.py
@@ -19,19 +19,25 @@ embeddings = OpenAIEmbeddings()
 def create_db_from_youtube_video_url(video_url):
     loader = YoutubeLoader.from_youtube_url(video_url)
     transcript = loader.load()
-
+#creates a transcript of the given youtube URL  
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
     docs = text_splitter.split_documents(transcript)
-
+# to manuplate the no of token limit  we use text splitter , basically to use OPEN AI api 
     db = FAISS.from_documents(docs, embeddings)
+    # embeddings convert all to those strings or text to number then FAISS is the library that peforms silmarity search for that doc we have just created
     return db
-
+# then return the desired database 
 
 def get_response_from_query(db, query, k=4):
     docs = db.similarity_search(query, k=k)
+#calculates the similarity between the query vector and the document vectors stored in the database. It returns the k most similar documents. k defaults to 4, meaning the top 4 most similar documents are retrieved.
     docs_page_content = " ".join([d.page_content for d in docs])
-
+#a single string containing the combined text content of those documents. This string is then typically used to provide context to a large language model (LLM) for answering the user's query.
     chat = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.2)
+#ai model initalised using chat based interface 
+
+
+
 
     # Template to use for the system message prompt
     template = """
@@ -53,11 +59,12 @@ def get_response_from_query(db, query, k=4):
     chat_prompt = ChatPromptTemplate.from_messages(
         [system_message_prompt, human_message_prompt]
     )
+#takes a list of message prompt templates (in this case, the system and human messages) and assembles them into a structured chat prompt understood by the LLMChain.
 
     chain = LLMChain(llm=chat, prompt=chat_prompt)
 
     response = chain.run(question=query, docs=docs_page_content)
-    response = response.replace("\n", "")
+    response = response.replace("\n", "")       
     return response, docs
 
 
